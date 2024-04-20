05e62cdbf860accefe5e90008164a275ec2fa7b7
Adrianfeloh1 <adrianfeloh1@gmail.com>
2024-04-20 10:01:12 -0500

    Iniciando funcionalidad del botón menú en tablet y mobile

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-/* Función para cambiar de imagen en la prueba 
-de manejo al dar click en cada botón */
 
-const changeModel = (model) => {
-    /* capturo el elemento img desde el id */
-  const imageModel = document.getElementById("modelImage"); 
+let navList;
+let imageModel;
+
+/* Función para cambiar de imagen en la prueba */
+
+window.onload = () => {
+  /* capturo el elementos */
+  navList = document.getElementById("navList")
+  imageModel = document.getElementById("modelImage");  
+} 
+
+/* Función para mostrar u ocultar la barra de navegación 
+al hacer clic en el botón de menú */
+
+const toggleMenu = () => {
+/*   Si la barra de navegación está visible, la oculta; 
+de lo contrario, la muestra
+ */  if (navList.style.display === "block") {
+    navList.style.display = "none";
+  } else {
+    navList.style.display = "block";
+  }
+}
+
+
+const changeImageModel = (model) => {    
   /* cambio el atributo de la ruta de la imagen */
   imageModel.src = "./assets/images/menu-" + model + ".avif";
 }
+
+
+
+
